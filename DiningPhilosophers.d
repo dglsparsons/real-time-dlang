7abd8a30b1094a59f0186d21c40503b5b17e3e84
Douglas Parsons <dglsparsons@gmail.com>
2015-09-30 09:02:08 +0100

    Tidied up and added an end condition

--- DiningPhilosophers.d
+++ DiningPhilosophers.d
@@ -2,72 +2,76 @@ import std.stdio;
 import std.concurrency; 
 import core.thread; 
 
+immutable int NUMBER_OF_SEATS = 5;
+immutable int PHILOSOPHER_EAT_COUNT = 5; 
+immutable int totalEatingCount = PHILOSOPHER_EAT_COUNT * NUMBER_OF_SEATS; 
+ 
+
 void main()
 {
-	Chopstick[5] chopsArray;
-	Tid[5] threadArray;  
-	for (int i = 0; i < 5; i++) { 
-		chopsArray[i] = new Chopstick(i); 
-	} 
-	// initialise all the philosophers
-	for (int i = 0; i < 5; i++) {
+	Chopstick[NUMBER_OF_SEATS] chopsArray;
+	Tid[NUMBER_OF_SEATS] threadArray;  
+	for (int i = 0; i < NUMBER_OF_SEATS; i++) {
+		chopsArray[i] = new Chopstick(); 
 		threadArray[i] = spawn(&philosopherThread, i);
 	}
-	// listen for a request or a release of chopsticks.
-	while(1) {
-		auto message = receiveOnly!(string,int,int);
-		auto command = message[0]; 
-		auto leftStick = message[1]; 
+	
+	int endCount = 0;
+	while ( endCount < totalEatingCount ) { // main loop
+		auto message 	= receiveOnly!(string,int,int);
+		auto command 	= message[0]; 
+		auto leftStick  = message[1]; 
 		auto rightStick = message[2];
-		 
+		
 		if (command == "request") {
-			// if both sticks are free, we can give them to him. 
+			// philosopher is requesting his chopsticks 
 			if (chopsArray[leftStick].free && chopsArray[rightStick].free) {
-				// mark the chopsticks as in use
 				chopsArray[leftStick].free = false; 
 				chopsArray[rightStick].free = false;
 				writeln("Given chopsticks ", leftStick," and ", rightStick, " to philosopher: ", leftStick);  
-				// signal that the philosopher can eat
 				threadArray[leftStick].send(true); 
 			} else {
-				threadArray[leftStick].send(false); 
+				threadArray[leftStick].send(false);
 			}
 		} else if (command == "return") {
+			// philosopher is returning his chopsticks
 			writeln("Philosopher ", leftStick, " has stopped eating");
 			chopsArray[leftStick].free = true; 
 			chopsArray[rightStick].free = true; 
+			endCount++;
 		}
 	}
+	thread_joinAll; 
 }
 
 void philosopherThread (int index)
 {
 	auto leftStick = index;
-	auto rightStick = (index+1) % 5;
-	// message the main thread asking for the sticks. 
-	for (int i = 0;; i++ ){
+	auto rightStick = (index+1) % NUMBER_OF_SEATS;
+	
+	int successfulEatCount; 
+	while(successfulEatCount < PHILOSOPHER_EAT_COUNT) {
 		bool canPickupForks = false;
-		// Think for a while 
-		Thread.sleep(200.msecs);   
-		while (!canPickupForks) {
-			ownerTid.send("request", leftStick, rightStick);
-			// wait for the response - letting us know we can eat. 
-			canPickupForks = receiveOnly!bool;
-		}
-		// now we have the chopsticks, eat.
-		writeln("Philosopher ", index, " is eating");  
-		Thread.sleep(200.msecs); 
-		// now give back the sticks
-		ownerTid.send("return", leftStick, rightStick);
+		// Philosopher thinks for a while 
+		Thread.sleep(200.msecs);
+		ownerTid.send("request", leftStick, rightStick);
+		// wait for the response - letting us know we can eat. 
+		canPickupForks = receiveOnly!bool;
+		if (canPickupForks) {
+			writeln("Philosopher ", index, " is eating");  
+			Thread.sleep(200.msecs); 
+			
+			ownerTid.send("return", leftStick, rightStick);
+			successfulEatCount++; 
+		} 
 	}
+	writeln("Philosopher: ", leftStick, " has finished eating for good!");
 }
 
 class Chopstick
 {
-	private int id; 
-	private bool free;
-	this(int index){
-		id = index; 
+	bool free;
+	this(){ 
 		free = true; 
 	}
 }
