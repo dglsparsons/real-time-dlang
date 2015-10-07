f5c8d66b40646a4def3c06c4857ace33e8ddbe0d
Douglas <dglsparsons@gmail.com>
2015-10-07 14:56:40 +0100

    Updated solution

--- PriorityThread.d
+++ PriorityThread.d
@@ -1,10 +1,16 @@
 import core.thread;
-import std.stdio;
 import core.sys.posix.sched; 
-import std.process; 
+import core.sys.posix.pthread; 
+import std.stdio;
 
 class AThread : Thread
 {
+    /*
+    __gshared const int PRIORITY_MIN = 1;
+    __gshared const int PRIORITY_MAX = 99;
+    __gshared const int PRIORITY_DEFAULT = 90; 
+    */
+	
     this()
     {
         super(&run);
@@ -13,15 +19,20 @@ class AThread : Thread
     private:
     void run()
     {
+        //This method of changing the priority works
+	this.priority(90); 
+        writeln("priority of A thread: ", this.priority); 
         foreach (number; 1..1000) {
-            //writefln("B: %s", number);
+            writeln("A: ", number, " ", this.priority);
         }
     }
 }
-
 
 class BThread : Thread
 {
+    //__gshared const int PRIORITY_MIN = 1;
+    //__gshared const int PRIORITY_MAX = 99;
+	
     this()
     {
         super(&run);
@@ -30,25 +41,34 @@ class BThread : Thread
     private:
     void run()
     {
+        writeln("priority of B thread: ", this.priority); 
         foreach (number; 1..1000) {
-            writefln("B: %s", number);
+            writeln("B: ", number, " ", this.priority);
         }
     }
 }
 
-
 void main()
 {
-    sched_param sp = { sched_priority : 50 };
+    writeln("initial scheduler: ", sched_getscheduler(0)); 
+    //Set the scheduler as FIFO, - this only works if run as sudo for some reason
+    sched_param sp = { sched_priority: 50 }; 
     int ret = sched_setscheduler(0, SCHED_FIFO, &sp); 
     if (ret == -1) {
-        writeln("crap");
+        throw new Exception("scheduler did not properly set");
     }
-    auto a = new AThread();
-    //a.priority = a.PRIORITY_MAX;
-    //auto b = new BThread();
-//    b.priority = b.PRIORITY_MIN;
-    a.start();
-    //b.start();
-}
+    writeln("new scheduler: ", sched_getscheduler(0)); 
 
+    //Check what the max and min priorities are
+    int max_prio = sched_get_priority_max(sched_getscheduler(0)); 
+    int min_prio = sched_get_priority_min(sched_getscheduler(0)); 
+    int default_prio = sched_get_priority(sched_getscheduler(0)); 
+    writeln("schedulers max_prio: ", max_prio, " min prio: ", min_prio, " default prio ", default_prio); 
+
+    //Create a new thread and check its MAX and MIN priorities
+    auto a = new AThread();
+    //writeln("PRIORITY_MIN: ", a.PRIORITY_MIN, " PRIORITY_MAX: ", a.PRIORITY_MAX); // These should be 1 and 99. But appear to be 0 and 0.
+
+    a.start();
+    auto b = new BThread().start(); 
+}
