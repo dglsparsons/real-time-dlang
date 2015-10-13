9286c46fc8c9fe30bd6038e7c887f575deecaf35
Douglas <dglsparsons@gmail.com>
2015-10-13 11:00:24 +0100

    Final version of preemption patch test

--- PreemptionPatchTest.d
+++ PreemptionPatchTest.d
@@ -4,20 +4,23 @@ import std.stdio;
 import core.thread; 
 import core.sys.posix.sched; 
 
+shared bool shouldExit = false; 
 
 void main()
 {
-    writeln("Program Started"); 
     sched_param sp = { sched_priority: 50 }; 
     int ret = sched_setscheduler(0, SCHED_FIFO, &sp); 
     if (ret == -1) {
         throw new Exception("scheduler did not properly set");
     }
     writeln("Starting threads"); 
-
+    
     new AThread().start(); 
+    Thread.sleep(1.seconds); 
     new BThread().start(); 
 
+    thread_joinAll; 
+    writeln("finished"); 
 }
 
 class AThread : Thread
@@ -29,10 +32,11 @@ class AThread : Thread
     private:
     void run()
     {
-        this.priority(90); 
+    	this.priority(90); 
         writeln("High Prio thread started!"); 
-        Thread.sleep(2.seconds); 
+        Thread.sleep(5.seconds); 
         writeln("High Prio thread has woken - preemption is working"); 
+        shouldExit = true; 
     }
 }
 
@@ -45,8 +49,9 @@ class BThread : Thread
     private: 
     void run()
     {
-        this.priority(51); 
-        while(1) {
+        this.priority(1); 
+        writeln("Low Priority thread started!"); 
+        while(!shouldExit) {
         }
     }
 }
