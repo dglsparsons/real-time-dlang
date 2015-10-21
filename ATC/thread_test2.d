667015501879f79c1bd269dead8cfa2fdfa69133
Douglas Parsons <dglsparsons@gmail.com>
2015-10-21 14:33:43 +0000

    Added sigaction_t variation of the thread_test

--- ATC/thread_test2.d
+++ ATC/thread_test2.d
@@ -6,7 +6,7 @@ import core.sys.posix.signal;
 import core.sys.posix.pthread; 
 import core.atomic; 
 
-shared pthread_t something; 
+shared pthread_t mythread; 
 shared int n = 0; 
 
 void main()
@@ -14,20 +14,22 @@ void main()
     auto a = new Thread(&myFunc); 
     a.start; 
     Thread.sleep(1.seconds); 
-    writeln("A: ", something); 
     while(1){
         Thread.sleep(5.seconds); 
-        pthread_kill(something, 34); 
+        pthread_kill(mythread, 34); 
     }
 }
 
 void myFunc()
 {
-    something = pthread_self();
-    signal(34, &handler); 
+    mythread = pthread_self();
+    sigaction_t action; 
+    action.sa_handler = &handler; 
+    sigemptyset(&action.sa_mask); 
+    sigaction(34, &action, null); 
     while(1)
     {
-        writeln("Thread doing smth: ", n); 
+        writeln("Thread doing work: ", n); 
         atomicOp!"+="(n, 1); 
         Thread.sleep(1.seconds); 
     }
