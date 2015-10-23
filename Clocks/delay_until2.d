107d183b3d05781ab92125da37b5c5a3410f476e
Douglas <dglsparsons@gmail.com>
2015-10-23 13:47:27 +0100

    Tidied Dlike version

---
+++ Clocks/delay_until2.d
@@ -0,0 +1,23 @@
+#!/usr/bin/rdmd
+
+import std.stdio; 
+import core.time; 
+
+void main()
+{
+    auto time = MonoTime.currTime; 
+    time += 3.seconds; 
+    delay_until(time); 
+    writeln("Delay Until is working!"); 
+}
+
+extern (C) void delay_until(MonoTime timeIn)
+{
+    import core.sys.linux.time; 
+    Duration dur = timeIn - MonoTime(0) ;
+    long secs, nansecs; 
+    dur.split!("seconds", "nsecs")(secs, nansecs); 
+    timespec sleep_time = timespec(secs, nansecs); 
+    if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleep_time, null))
+        throw new Exception("Failed to sleep as expected!"); 
+}
