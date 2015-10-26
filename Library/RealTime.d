3202d940ba4cace7b09303e5ebf25f0a3baa0557
Douglas Parsons <dglsparsons@gmail.com>
2015-10-26 12:00:52 +0000

    Updated and added unit tests

--- Library/RealTime.d
+++ Library/RealTime.d
@@ -19,13 +19,15 @@ import core.time;
 
 void delay_until(MonoTime timeIn)
 {
-    import core.sys.linux.time; 
-    Duration dur = timeIn - MonoTime(0) ;
-    long secs, nansecs; 
-    dur.split!("seconds", "nsecs")(secs, nansecs); 
-    timespec sleep_time = timespec(secs, nansecs); 
-    if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleep_time, null))
-        throw new Exception("Failed to sleep as expected!"); 
+    version(Posix) {
+        import core.sys.linux.time; 
+        Duration dur = timeIn - MonoTime(0) ;
+        long secs, nansecs; 
+        dur.split!("seconds", "nsecs")(secs, nansecs); 
+        timespec sleep_time = timespec(secs, nansecs); 
+        if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleep_time, null))
+            throw new Exception("Failed to sleep as expected!"); 
+    }
 }
 
 
@@ -50,32 +52,47 @@ void delay_until(MonoTime timeIn)
  *  Note:
  *  Depending on the distribution of linux, the program may require root
  *  priveledges in order to change its scheduler. 
+ *  Also: Testing on Arch Linux, it appears SCHED_BATCH and SCHED_IDLE are not
+ *  present?
  */
 
 void setScheduler(int scheduler_type, int scheduler_priority)
 {
-    import core.sys.posix.sched; 
-    sched_param sp = { sched_priority: scheduler_priority }; 
-    int ret = sched_setscheduler(0, scheduler_type, &sp); 
-    if (ret == -1) {
-        throw new Exception("scheduler did not properly set");
+    version(Posix){
+        import core.sys.posix.sched; 
+        sched_param sp = { sched_priority: scheduler_priority }; 
+        int ret = sched_setscheduler(0, scheduler_type, &sp); 
+        if (ret == -1) {
+            throw new Exception("scheduler did not properly set");
+        }
     }
 }
 
-
+unittest 
+{
+    import core.sys.posix.sched; 
+    setScheduler(SCHED_FIFO, 50); 
+    auto a = sched_getscheduler(0); 
+    assert(a == SCHED_FIFO); 
+    setScheduler(SCHED_RR, 50); 
+    a = sched_getscheduler(0); 
+    assert(a == SCHED_RR); 
+    /*
+    setScheduler(SCHED_BATCH); 
+    a = sched_getscheduler(0); 
+    assert(a == SCHED_BATCH); 
+    setScheduler(SCHED_IDLE); 
+    a = sched_getscheduler(0); 
+    assert(a == SCHED_IDLE); 
+    */
+}
 
 // TODO - THREAD FIXES NEEDED - Thread.PRIORITY_MAX and Thread.PRIORITY_MIN are not
-// working. In addition it is not possible to set the priority of a thread
-// until it is running - This needs fixing??? -- PRIORITY_MAX and PRIORITY_MIN
-// are in an set of code that never gets called for POSIX? - BUG IN THE
-// RUNTIME?
+// being correctly set. In addition it is not possible to set the priority of a thread
+// until it is running - Does this need fixing? 
 
-
-// There might be a tidier way to implement a Mutex with Priority Inheritance
-// or Priority Inversion than this. It is quite messy. :(
-
-immutable int PRIORITY_INHERIT = 0; 
-immutable int PRIORITY_CEILING = 1; 
+enum PRIORITY_INHERIT = 2; 
+enum PRIORITY_CEILING = 3; 
 
 /** 
  *  The following function provides additional functionality compared to the
@@ -88,16 +105,14 @@ immutable int PRIORITY_CEILING = 1;
  *  Params: 
  *  int type = This determines whether the mutex created will use the priority
  *  inheritance or the priority ceiling protocol. Values can be
- *  PRIORITY_INHERIT or PRIORITY_CEILING. 
+ *  PRIORITY_INHERIT or PRIORITY_CEILING. Passing in 0 will initialise the
+ *  mutex with neither protocol. 
  *
  *  Example: 
  *  ---
  *  auto a = new RTMutex(PRIORITY_INHERIT); 
  *  ---
  *
- *  Note:
- *  TODO - Provide a nice interface for getting and setting the priority
- *  ceiling of the mutex, if using the ceiling protocol. 
  */
 
 import core.sync.mutex; 
@@ -111,27 +126,20 @@ class RTMutex : Mutex
             pthread_mutexattr_t attr = void; 
 
             if(pthread_mutexattr_init(&attr))
-            {
                 throw new SyncError("Unable to initialize Mutex"); 
-            }
 
             if(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE))
-            {
                 throw new SyncError("Unable to initialize mutex");
-            }
-            if(type)
+
+            if(type == PRIORITY_CEILING)
             {
                 if(pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_PROTECT))
-                {
                     throw new SyncError("Unable to initialize Priority ceiling protocol"); 
-                }
             }
-            else 
+            else if (type == PRIORITY_INHERIT)
             {
                 if(pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_INHERIT))
-                {
                     throw new SyncError("Unable to initialize Priority inheritance protocol"); 
-                }
             }
 
             if( pthread_mutex_init(&m_hndl, &attr))
@@ -140,6 +148,61 @@ class RTMutex : Mutex
             }
         }
     }
+
+    /**
+     * Gets the priority ceiling for the associated mutex. 
+     * 
+     * Returns: 
+     *  The priority ceiling of this thread.
+     * 
+     * Example: 
+     * ---
+     * auto a = new RTMutex(PRIORITY_CEILING); 
+     * int currentPriorityCeiling = a.ceiling();
+     * ---
+     */
+    final @property int ceiling()
+    {
+        version(Posix)
+        {
+            int ceiling; 
+            if(pthread_mutex_getprioceiling(&m_hndl, &ceiling))
+                throw new SyncError("Unable to fetch the priority ceiling for the associated Mutex"); 
+            return ceiling; 
+        }
+    }
+
+    /**
+     * Sets the priority ceiling for the associated mutex. 
+     * 
+     * Params: 
+     *  val = The new priority ceiling of this mutex
+     *
+     * Example: 
+     * ---
+     * auto a = new RTMutex(PRIORITY_CEILING); 
+     * int newCeiling = 50; 
+     * a.ceiling(newCeiling); 
+     * ---
+     */
+    final @property void ceiling(int val)
+    {
+        version(Posix)
+        {
+            if(pthread_mutex_setprioceiling(&m_hndl, val, null))
+                throw new SyncError("Unable to set the priority ceiling for the associated Mutex"); 
+        }
+    }
 }
 
-
+unittest 
+{
+    auto a = new RTMutex(PRIORITY_CEILING); 
+    int prio = 50; 
+    a.ceiling(prio); 
+    assert(prio == a.ceiling); 
+    int newPrio = 25; 
+    a.ceiling(newPrio); 
+    assert(prio != a.ceiling); 
+    assert(newPrio == a.ceiling); 
+}
