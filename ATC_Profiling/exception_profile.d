c53bbbbd8457bbdeba923b80334129ea96f61465
Douglas Parsons <dglsparsons@gmail.com>
2016-04-23 14:51:15 +0000

    Updated with standard deviation. Required for the writeup.

--- ATC_Profiling/exception_profile.d
+++ ATC_Profiling/exception_profile.d
@@ -1,5 +1,5 @@
 import realtime, interruptible_exception, 
-       core.time, std.stdio, core.thread;
+       core.time, std.stdio, core.thread, std.math, core.memory;
 
 immutable auto numtests = 10_000;
 
@@ -10,17 +10,25 @@ void main()
 
     Duration totalSetup; 
     Duration totalTeardown;
+    Duration setupTimes[numtests];
+    Duration teardownTimes[numtests];
 
     for (auto i = 0; i < numtests; i++)
     {
+        GC.collect;
         MonoTime timeCancelled;
         MonoTime timeAfter;
         MonoTime timeStarting; 
+
         auto timeBefore = MonoTime.currTime;
 
         auto intr = new Interruptible({
+            getInt.deferred = true;
             timeStarting = MonoTime.currTime;
-            while(true){}
+            while(true)
+            {
+                getInt.testCancel;
+            }
         });
 
         new Thread({
@@ -32,10 +40,27 @@ void main()
         intr.start;
         timeAfter = MonoTime.currTime; 
 
-        totalSetup += timeStarting - timeBefore;
-        totalTeardown += timeAfter - timeCancelled;
+        setupTimes[i] = timeStarting - timeBefore;
+        teardownTimes[i] = timeAfter - timeCancelled;
+        totalSetup += setupTimes[i]; 
+        totalTeardown += teardownTimes[i];
     }
 
     writeln("Average Setup:    ", totalSetup / numtests); 
     writeln("Average Teardown: ", totalTeardown / numtests); 
+
+    double totalVariance = 0; 
+    foreach(time; setupTimes)
+    {
+        totalVariance += (time - (totalSetup/numtests)).abs.total!"usecs".pow(2);
+    }
+    writeln("Standard deviation of setup: ", sqrt(totalVariance /numtests));
+
+    totalVariance = 0;
+
+    foreach(int i, time; teardownTimes)
+    {
+        totalVariance += (time - (totalSetup/numtests)).abs.total!"usecs".pow(2);
+    }
+    writeln("Standard deviation of teardown: ", sqrt(totalVariance /numtests));
 }
